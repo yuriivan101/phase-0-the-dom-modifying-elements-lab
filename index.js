b3373fb7f00ad8f57a79be56acea2a966cca1955
Yuriy <ivanenko.yuri@gmail.com>
2024-02-24 18:31:09 -0500

    Passed DOM tests

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const main = document.querySelector('#main')
+document.body.removeChild(main)
+
+const newHeader = document.createElement('h1')
+newHeader.setAttribute('id','victory')
+newHeader.textContent = "Yuriy is the champion"
+document.body.appendChild(newHeader)
